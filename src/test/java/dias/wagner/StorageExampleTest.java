271034d61e9c1988255be596214bfd565e54bd15
dias-wagner <dias.wagner@gmail.com>
2018-01-08 08:14:46 -0200

    First commit with test

--- src/test/java/dias/wagner/StorageExampleTest.java
+++ src/test/java/dias/wagner/StorageExampleTest.java
@@ -1,4 +1,11 @@
 package dias.wagner;
 
+import org.junit.Test;
+
 public class StorageExampleTest {
+
+    @Test
+    public void testDownload() throws Exception {
+        StorageExample.main("download", "gcloud-storage-test", "textos-artistas.txt", "/tmp/textos-artistas.txt");
+    }
 }
